67279cb1d18287505004218b5349f7779077cdc3
sk <Sanjay.k@live.com>
2018-02-14 18:43:45 +0530

    Functions
    Added Separate functions for getting data and printing data

--- gmaps.py
+++ gmaps.py
@@ -12,27 +12,33 @@ from pprint import pprint
 #My Google API Key. 
 gmaps = googlemaps.Client(key='AIzaSyDC7hNFXWum48OYliVbKbH0RAIPGieejDA')
 
-#Get the location for which the cordinates are required.
-location = input('Enter a location to find its cordinates: ')
 
-#use the geocode() to get the detailed information about the location.
-geocode_result = gmaps.geocode(location)
+def print_location_data(data):
+    #For debugging.
+    #pprint(geocode_result[0]['geometry']['location']['lat'])
+    #print(geocode_result[0]['geometry']['location']['lng'])
 
-#For debugging.
-#pprint(geocode_result[0]['geometry']['location']['lat'])
-#print(geocode_result[0]['geometry']['location']['lng'])
+    if(data==[]):
+        print("Error. No such location found.")
+    else:
+        #pprint(geocode_result)
+        latitude = data[0]['geometry']['location']['lat']
+        longitude = data[0]['geometry']['location']['lng']
+        print("The cordinates of {} are \nLatitude = {} \nLongitude = {}".format(location, latitude, longitude))
 
-if(geocode_result==[]):
-    print("Error. No such location found.")
-else:
-    #pprint(geocode_result)
-    latitude = geocode_result[0]['geometry']['location']['lat']
-    longitude = geocode_result[0]['geometry']['location']['lng']
-    print("The cordinates of {} are \nLatitude = {} \nLongitude = {}".format(location, latitude, longitude))
+def main():
+    #Get the location for which the cordinates are required.
+    location = input('Enter a location to find its cordinates: ')
 
+    #use the geocode() to get the detailed information about the location.
+    geocode_result = gmaps.geocode(location)
+
+    #print data.
+    print_location_data(geocode_result)
     
-
-
+if __name__==__main__:
+    main()
+    
 
 
 
